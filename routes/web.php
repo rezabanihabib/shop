b321f2045359a024710413dea1c14326a89c5ae1
Reza Banihabib <rezabanihabib20@gmail.com>
2025-07-13 15:36:17 +0330

    web.php Updated
    Main Page Routes Name Added.
    
    Temporary Logout Added.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'client.index');
+Volt::route('/', 'client.index')->name('home');
 
 //AUTH ROUTES
 Route::middleware('guest')->group(function () {
@@ -12,6 +14,15 @@ Route::middleware('guest')->group(function () {
     Volt::route('forgot-password', 'auth.forgot-password')->name('password.request');
     Volt::route('reset-password/{token}', 'auth.reset-password')->name('password.reset');
 });
+
+Route::post('logout', function () {
+    Auth::guard('web')->logout();
+
+    Session::invalidate();
+    Session::regenerateToken();
+
+    return redirect('/');
+})->name('logout');
 
 //Dashboard ROUTES
 Route::middleware('auth')->group(function () {
