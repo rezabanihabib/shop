cac62d73d7e947cdded613f852cb10bcf066826e
Reza Banihabib <rezabanihabib20@gmail.com>
2025-07-13 15:04:55 +0330

    Authentication Routes Refactored
    Login Route Added.
    
    Forgot Password Route Added.
    
    Reset Password Route Added.

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ Volt::route('/', 'client.index');
 //AUTH ROUTES
 Route::middleware('guest')->group(function () {
     Volt::route('register', 'auth.register')->name('register');
+    Volt::route('login', 'auth.login')->name('login');
+    Volt::route('forgot-password', 'auth.forgot-password')->name('password.request');
+    Volt::route('reset-password/{token}', 'auth.reset-password')->name('password.reset');
 });
