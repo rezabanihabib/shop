f548cacc91baa44e2c6c39694f81c5455a07dd46
Reza Banihabib <rezabanihabib20@gmail.com>
2025-07-13 14:47:23 +0330

    Main Page Refactored
    resources/livewire/client/index.blade.php created.
    
    Main Page Route Refactored.
    
    Main Page View Refactored.
    
    App Layout View Refactored.

---
+++ resources/views/livewire/client/index.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+use Livewire\Volt\Component;
+
+new
+#[Layout('components.layouts.app')]
+#[Title('خانه')]
+class extends Component 
+{
+    //
+}; 
+
+?>
+
+<div>
+    
+</div>
