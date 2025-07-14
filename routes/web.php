0eb42638b34568e5bc0fd4609d40d59065c9d35e
Reza Banihabib <rezabanihabib20@gmail.com>
2025-07-14 13:10:16 +0330

    web.php Refactored
    Client, Admin, Dashboard Base Routes Group Added.
    
    Reset Password, Verify Email, Confirm Password Routes Added.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,20 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'client.index')->name('home');
+//ADMIN ROUTES
+Route::middleware(['auth', 'password.confirm'])->prefix('admin/')->name('admin.')->group(function () {
+    Route::get('/', 'index')->name('index');
+});
+
+//ClIENT ROUTES
+Route::prefix('client/')->name('client.')->group(function () {
+    Route::get('/', 'index')->name('index');
+});
+
+//DASHBOARD ROUTES
+Route::middleware(['auth', 'password.confirm'])->prefix('dashboard/')->name('dashboard.')->group(function () {
+    Route::get('/', 'index')->name('index');
+});
 
 //AUTH ROUTES
 Route::middleware('guest')->group(function () {
@@ -13,6 +26,14 @@ Route::middleware('guest')->group(function () {
     Volt::route('/login', 'auth.login')->name('login');
     Volt::route('/forgot-password', 'auth.forgot-password')->name('password.request');
     Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
+});
+
+Route::middleware('auth')->group(function () {
+    Volt::route('verify-email', 'auth.verify-email')->name('verification.notice');
+
+    // Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+
+    Volt::route('confirm-password', 'auth.confirm-password')->name('password.confirm');
 });
 
 Route::post('/logout', function () {
@@ -23,8 +44,3 @@ Route::post('/logout', function () {
 
     return redirect('/');
 })->name('logout');
-
-//Dashboard ROUTES
-Route::middleware('auth')->group(function () {
-    Volt::route('/dashboard', 'dashboard.index')->name('dashboard');
-});
