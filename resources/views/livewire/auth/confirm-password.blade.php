1f2adab75a464aff295eb6b2435a21ff538403ac
Reza Banihabib <rezabanihabib20@gmail.com>
2025-07-14 13:26:55 +0330

    Dashboard And Main Page Routes Updated.
    Dashboard Route From dashboard to dasboard.index Updated.
    
    Main Page Routes From Home to client.index Udated.

--- resources/views/livewire/auth/confirm-password.blade.php
+++ resources/views/livewire/auth/confirm-password.blade.php
@@ -32,7 +32,7 @@ new #[Layout('components.layouts.auth')] class extends Component
 
         session(['auth.password_confirmed_at' => time()]);
 
-        $this->redirectIntended(default: route('dashboard', absolute: false), navigate: true);
+        $this->redirectIntended(default: route('dashboard.index', absolute: false), navigate: true);
     }
 };
 ?>
